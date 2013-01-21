8d1a6b5b7e52eb0bbc6a236106680d2884454dc5
Kamil Wysocki <wysocki.km@gmail.com>
2013-01-21 18:27:24 +0100

    start projektu ;)

---
+++ MySuperApp/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace MySuperApp.Controllers
+{
+    public class UserController : ApiController
+    {
+    }
+}
